34359eeadf5581a28af1260e099131c177ceff04
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-20 15:21:44 +0530

    terraform

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -103,6 +103,16 @@ resource "aws_security_group_rule" "frontend_bastion" {
   security_group_id = module.frontend_sg.id
 }
 
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.id
+}
+
+
 resource "aws_security_group_rule" "mysql_ansible" {
   type              = "ingress"
   from_port         = 22
